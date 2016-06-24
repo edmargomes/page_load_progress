b4cf554068a7cca29638b7a0d4d7346c6672360d
Aurelien Navarre <aurelien.navarre@acquia.com>
2016-06-24 15:41:33 +0200

    Convert more arrays with short syntax

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -34,25 +34,25 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $page_load_progress_config = $this->config('page_load_progress.settings');
 
-    $form['page_load_progress_time'] = array(
+    $form['page_load_progress_time'] = [
       '#type' => 'select',
       '#title' => t('Time to wait before locking the screen'),
       '#description' => t('Enter the time you want to wait before showing the image lock.'),
-      '#options' => array(
+      '#options' => [
         10   => t('Show immediately'),
         1000 => t('Show after a 1 second delay'),
         3000 => t('Show after a 3 seconds delay'),
         5000 => t('Show after a 5 seconds delay'),
-      ),
+      ],
       '#default_value' => $page_load_progress_config->get('page_load_progress_time'),
-    );
+    ];
 
-    $form['page_load_progress_elements'] = array(
+    $form['page_load_progress_elements'] = [
       '#type' => 'textfield',
       '#title' => t('Elements that will trigger the throbber'),
       '#description' => t('Enter the elements that will trigger the effect separated by commas. Classes should have their leading dot and ids their leading hashes.'),
       '#default_value' => $page_load_progress_config->get('page_load_progress_elements'),
-    );
+    ];
 
     return parent::buildForm($form, $form_state);
   }
